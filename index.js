42609a04d03b8c3677763ed87d2933a03c8d729f
Rubembraga <70537314+Rubembraga@users.noreply.github.com>
2022-09-21 09:54:13 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+var nota1 = 8
+var nota2 = 9
+var nota3 = 7
+var media = (nota1+nota2+nota3)/3
+console.log("Sua média é: ", media)
+var resultado = media>=7?("Aprovado"):("Reprovado")
+console.log(resultado)
